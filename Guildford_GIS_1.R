64ee5f18c51b6a31c00b44cd657fb15125650ee3
Derek Data Science <rtpopendata@gmail.com>
2022-01-16 22:38:46 -0500

    Add files via upload

--- Guildford_GIS_1.R
+++ Guildford_GIS_1.R
@@ -9,7 +9,36 @@ library(ggplot2)
 library(sf)
 library(maps)
 
+if(!require(pacman))install.packages("pacman")
+pacman::p_load('dplyr', 'tidyr', 'gapminder',
+               'ggplot2', 'ggalt',
+               'forcats', 'R.utils','png',
+               'grid','ggpubr','scales',
+                'bbplot')
 
+devtools::install_github('bbc/bbplot', force=TRUE)
+
+line_df <- gapminder %>%
+  filter(country == "Malawi")
+
+line <- ggplot(line_df, aes(x = year, y = lifeExp)) +
+  geom_line(colour = "#1380A1", size = 1 ) +
+  geom_hline(yintercept = 0, size = 1, colour = "#333333") +
+  bbc_style() +
+  labs(title = "Living longer",
+       subtitle = "Life expetancy in Malawi 1952-2007")
+line
+
+theme(panel.grid.major.x = element_line(colour="#cbcbcb"),
+      panel.gri.major.y=element_blank())
+
+finalise_plot(plot_name = my_line_plot,
+              source = "Source: Gapminder",
+              save_filepath="file1.png",
+              width_pixes = 640,
+              height_pixels = 450
+              #logo_image_path = "placeholder.png"
+              )
 
 file.exists('C:/Dev/Datasets/Guilford_Addressing_shp/Addr_Points_010421.shp')
 map <- readOGR(dsn="C:/Dev/Datasets/Guilford_Addressing_shp",layer="Addr_Points_010421",verbose=FALSE)
@@ -31,6 +60,82 @@ map_wgs84 <- spTransform(map3, CRS("+proj=longlat +datum=WGS84"))
 write.csv(map_wgs84, "C:/Dev/Datasets/Contour_100ft.csv",row.names=TRUE)
 str(map_wgs84)
 plot(map_wgs84, axes=TRUE)
+
+install.packages("rgdal")
+install.packages("rgeos")
+install.packages("maptools")
+library(rgdal)
+library(rgeos)
+library(ggplot2)
+
+
+
+file.exists('../GIS/gis_osm_natural_a_free_1.shp')
+map <- readOGR(dsn="../GIS",layer="gis_osm_natural_a_free_1",verbose=FALSE)
+map_wgs84 <- spTransform(map, CRS("+proj=longlat +datum=WGS84"))
+#str(map_wgs84)
+#summary(map_wgs84)
+write.csv(map_wgs84, "../GIS/gis_osm_natural_a_free_2.csv", row.names=TRUE)
+summary(map_wgs84)
+plot(map_wgs84, axes=TRUE)
+
+file.exists('C:/Dev/downloads/Addr_Points_010421/Addr_Points_010421.shp')
+map <- readOGR(dsn="C:/Dev/downloads/Addr_Points_010421",layer="Addr_Points_010421",verbose=FALSE)
+map_wgs84 <- spTransform(map, CRS("+proj=longlat +datum=WGS84"))
+#str(map_wgs84)
+#summary(map_wgs84)
+write.csv(map_wgs84, "C:/Dev/downloads/Addr_Points_010421.csv", row.names=TRUE)
+summary(map_wgs84)
+plot(map_wgs84, axes=TRUE)
+
+file.exists('C:/Dev/Datasets/GIS/gis_osm_places_a_free_1.shp')
+map <- readOGR(dsn="C:/Dev/Datasets/GIS",layer="gis_osm_places_a_free_1",verbose=FALSE)
+map_wgs84 <- spTransform(map, CRS("+proj=longlat +datum=WGS84"))
+#str(map_wgs84)
+#summary(map_wgs84)
+write.csv(map_wgs84, "C:/Dev/Datasets/gis_osm_places_a_free_2.csv", row.names=TRUE)
+summary(map_wgs84)
+plot(map_wgs84, axes=TRUE)
+
+file.exists('C:/Dev/Datasets/GIS/gis_osm_buildings_a_free_1.shp')
+map <- readOGR(dsn="C:/Dev/Datasets/GIS",layer="gis_osm_buildings_a_free_1",verbose=FALSE)
+map_wgs84 <- spTransform(map, CRS("+proj=longlat +datum=WGS84"))
+#str(map_wgs84)
+#summary(map_wgs84)
+write.csv(map_wgs84, "C:/Dev/Datasets/gis_osm_buildings_a_free_2.csv", row.names=TRUE)
+summary(map_wgs84)
+plot(map_wgs84, axes=TRUE)
+
+file.exists('C:/Dev/Datasets/GIS/nc.shp')
+map <- readOGR(dsn="C:/Dev/Datasets/GIS",layer="nc",verbose=FALSE)
+map_wgs84 <- spTransform(map, CRS("+proj=longlat +datum=WGS84"))
+#str(map_wgs84)
+#summary(map_wgs84)
+write.csv(map_wgs84, "C:/Dev/Datasets/nc2.csv", row.names=TRUE)
+summary(map_wgs84)
+plot(map_wgs84, axes=TRUE)
+
+
+
+write.csv(map_wgs84, "C:/Dev/Datasets/Guildford_Addressing.csv", row.names=TRUE)
+plot(map_wgs84, axes=TRUE)
+
+map2 <- readOGR(dsn="C:/Dev/Datasets/GCVEC_StatePlane",layer="GCVEC_StatePlaneGrid200", verbose=FALSE)
+map_wgs84 <- spTransform(map2, CRS("+proj=longlat +datum=WGS84"))
+write.csv(map_wgs84, "C:/Dev/Datasets/GCVEC_StatePlaneGrid200.csv",row.names=TRUE)
+summary(map_wgs84)
+plot(map_wgs84, axes=TRUE)
+
+map3 <- readOGR(dsn="C:/Dev/Datasets/Contour_100ft",layer="Contour_100ft", verbose=FALSE)
+map_wgs84 <- spTransform(map3, CRS("+proj=longlat +datum=WGS84"))
+write.csv(map_wgs84, "C:/Dev/Datasets/Contour_100ft.csv",row.names=TRUE)
+str(map_wgs84)
+plot(map_wgs84, axes=TRUE)
+
+map4 <- readOGR(dsn="c:/Dev/Datasets/")
+
+
+
 
 map4 <- readOGR(dsn="c:/Dev/Datasets/")
 
