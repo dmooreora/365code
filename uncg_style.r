f241cf4713c6ff8601e4399ce6d5e5f9ed692b18
Derek Data Science <rtpopendata@gmail.com>
2022-01-19 00:20:12 -0500

    Add files via upload

---
+++ uncg_style.r
@@ -0,0 +1,14 @@
+#Add UNCG theme to ggplot charts
+#
+# This function allows you to add the UNCG theme to your ggplot grpahics
+# @keywords uncg_style
+# @export
+# @examples
+# line <- ggplot(line_df, aes(x = year, y = lifeExp)) +
+# geom_line(colour = "#007f7f)", size = 1) +
+# geom_hline(yintercept = 0, size = 1, colour = "#333333") +
+# uncg_style()
+
+uncg_style <- function() {
+  font <- "Helvetica"
+}
